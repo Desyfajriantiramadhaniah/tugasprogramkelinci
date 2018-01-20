2df7ced923e71541a1efb4cb0d3c1c1005584fe9
desyfajrianti <dessy.kps@gmail.com>
2018-01-20 15:46:43 +0800

    class kelinci

---
+++ Untitled1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Kelinci{
+	public:
+		void jenis(string jenis);
+		string warnaBulu;
+		string jenisKelinci;
+		string makananKelinci;
+		string warnaMata;
+};
+
+int main(){
+}
+
